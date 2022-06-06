cb5fbdde57eb5d7d63196df075cba8adf2a072a7
Eduardo Argenti <eduardosilvargenti@gmail.com>
2022-06-06 20:09:35 -0300

    Aula 58 - Melhorando o visual do aplicativo.

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -10,7 +10,7 @@ class TesteController extends Controller
         //echo $p1 . ' + ' . $p2 . ' = ' . ($p1 + $p2);
 
         //return view('site.teste', ['p1' => $p1, 'p2' => $p2]); // Array Associativo
-        //return view('site.teste', compact('p1', 'p2')); // Compact (nativo do PHP, o método mais comum)
-        return view('site.teste')->with('p1', $p1)->with('p2', $p2); // With
+        return view('site.teste', compact('p1', 'p2')); // Compact (nativo do PHP, o método mais comum)
+        //return view('site.teste')->with('p1', $p1)->with('p2', $p2); // With
     }
 }
---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,7 @@
+<h3>Fornecedores</h3>
+
+{{-- Este é um exemplo de comentário na sintaxe do Blade --}} 
+
+@php
+    // Podemos usar a sintaxe nativa do PHP nesses trechos @php
+@endphp

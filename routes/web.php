b8e0100b3530e4915f8d7cfb5fcb4385abbf557f
Eduardo Argenti <eduardosilvargenti@gmail.com>
2022-06-06 19:01:43 -0300

    Aula 34 - Criando o menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,3 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function (
-        string $nome = 'Não informado',
-        int $categoria_id = 1
-    ) {
-        echo 'Nome: ' . $nome . '<br>Categoria: ' . $categoria_id;
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');

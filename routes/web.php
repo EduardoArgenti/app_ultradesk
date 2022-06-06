7865fc38b1082620877cb90bcf7b1b5fd55b41e0
Eduardo Argenti <eduardosilvargenti@gmail.com>
2022-06-06 18:55:15 -0300

    Aula 33 - Tratando parâmetros de rotas com expressões regulares.

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,11 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
 
 Route::get(
-    '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+    '/contato/{nome}/{categoria_id}',
     function (
         string $nome = 'Não informado',
-        string $categoria = 'Não informado',
-        string $assunto = 'Não informado',
-        string $mensagem = 'Não informado'
+        int $categoria_id = 1
     ) {
-        echo 'Nome: ' . $nome . '<br>Categoria: ' . $categoria . '<br>Assunto: ' . $assunto . '<br>Mensagem: ' . $mensagem;
+        echo 'Nome: ' . $nome . '<br>Categoria: ' . $categoria_id;
     }
-);
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
